0321c0dcb78df9a95396f14200b11e95c9a87ae7
ismile-mzaalo <ismile@mzaalo.com>
2023-07-28 15:48:27 +0530

    build update

--- src/api/backendApi.js
+++ src/api/backendApi.js
@@ -1,9 +1,9 @@
 let host = "https://joyous-shawl-dog.cyclic.app"; //"http://localhost:8000";
 
 export const Apis = {
-  UserLogin: host + "/api/user/login",
-  GetUsers: host + "/api/user",
-  GetCourse: host + "/api/user/course",
-  CreateCourse: host + "/api/user/course",
-  CreateLecture: host + "/api/user/course/",
+  UserLogin: "/api/user/login",
+  GetUsers: "/api/user",
+  GetCourse: "/api/user/course",
+  CreateCourse: "/api/user/course",
+  CreateLecture: "/api/user/course/",
 };
